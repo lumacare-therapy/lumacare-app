b48e07d15f107d8fd8e7ce14aa02753b9e72a9df
lumacare-therapy <lumacare.therapy@gmail.com>
2026-05-24 21:34:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const paypalClientId = 'AYkkL5gWX0Ipl01CqNjK5el4DwNNkzo9BHuI991fm8hmonNNlMtZ_2RBRISsJbsiMmgCwLJyJbwoP8eD';
-
 root.render(
   <React.StrictMode>
-    <PayPalScriptProvider options={{ 
-      "client-id": paypalClientId,
-      currency: "USD",
-      intent: "subscription",
-      vault: true
-    }}>
-      <App />
-    </PayPalScriptProvider>
+    <App />
   </React.StrictMode>
 );
 
